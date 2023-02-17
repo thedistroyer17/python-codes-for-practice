6d892db63e46de6460ab8553f0aa120e26cea64e
darkdevs <arpit2003y@gmail.com>
2023-02-17 13:28:19 +0530

    comments added

--- program2.py
+++ program2.py
@@ -1,5 +1,9 @@
 # Python program to display the current date and time.
+# importing date tim module
 import datetime
+#storing date time in now variable 
 now = datetime.datetime.now()
+# string added 
 print("date and time right now :")
+# specifictaion of format of time 
 print(now.strftime("%d-%m-%Y  %H:%M:%S"))
